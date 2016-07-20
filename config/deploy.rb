1288e6174df26d5e53204fa5d702c6936d6c4684
Omair <omair@gems.techverx.com>
2016-07-21 03:34:50 +0500

    aws

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,23 @@ set :puma_workers,    0
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ec2/id_rsa_personal.pem) }
+#set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ec2/id_rsa_personal.pem) }
+
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
