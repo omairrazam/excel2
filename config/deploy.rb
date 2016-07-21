10d3e0b7932c0f66b9f80aa688e5d566da2d4c23
Omair <omair@gems.techverx.com>
2016-07-22 02:04:52 +0500

    database.yml added to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :linked_files,    %w{config/database.yml config/application.yml}
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
