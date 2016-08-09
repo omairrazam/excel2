40f76953731b2737203fbb220b243fa773755733
Omair <omair@gems.techverx.com>
2016-08-09 12:31:54 +0500

    async sidekick removed when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,8 @@ class User < ActiveRecord::Base
     create_machine("SHEL003")
     create_machine("SHEL004")
     create_machine("SHEL005")
-    PygmentsWorker.perform_async(1)
+    #PygmentsWorker.perform_async(1)
+    update
   end
 
   private    
@@ -27,4 +28,28 @@ class User < ActiveRecord::Base
     m1.name = machine_name
     m1.save
   end 
+
+  def update
+    users = User.all
+    users.each do |user|
+      directory_name = Rails.root.to_s +  "/excelsheets"
+      Dir.mkdir(directory_name) unless File.exists?(directory_name)
+      session = GoogleDrive::Session.from_config("config.json")
+
+      if user.sheet_name.present?
+        ws = session.spreadsheet_by_title(user.sheet_name).worksheets[0] rescue nil
+        user_machines = user.machines
+
+        if user_machines.present? and ws.present?
+          Machine.fetch_data_from_excel(ws, user, user_machines)
+          user_machines.each do|machine|
+            machine.update_offtimes
+            machine.touch
+          end
+        end
+
+      end
+
+    end
+  end
 end
