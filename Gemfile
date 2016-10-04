ac467e09b8dc599f247a03c1fe725d2b6b18b6f5
Omair <omair@gems.techverx.com>
2016-10-04 21:40:00 +0500

    aws issue

--- Gemfile
+++ Gemfile
@@ -59,7 +59,7 @@ group :production do
   gem 'pg', '0.18.4'
 end
 
-#gem 'figaro'
+gem 'figaro'
 group :development do
     gem 'capistrano',         require: false
     gem 'capistrano-rvm',     require: false
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,62 +1,23 @@
-# config valid only for current version of Capistrano
-
-#server '52.25.85.42', roles: [:web, :app, :db], primary: true
-
-
 
 set :application, 'excel2'
 set :repo_url, 'git@github.com:omairrazam/excel2.git'
-set :user, 'deploy'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
 
-# set :sidekiq_role, :app  
-# set :sidekiq_env, 'production'  
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/home/deploy/data"
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
- set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
- set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 set :branch, :v2
 set :deploy_to, '/home/deploy/excel2'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads }
 set :keep_releases, 5
 set :rvm_type, :user
-
+#set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
+set :bundle_binstubs, nil
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
 set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_error.log"
-set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_error_log, "#{shared_path}/log/puma_error.log"
+set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 set :puma_threads, [0, 8]
@@ -64,35 +25,22 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 
 
 
 
-namespace :deploy do
+# namespace :deploy do
+#   desc 'Runs rake db:seed for SeedMigrations data'
+#   task :seed => [:set_rails_env] do
+#     on primary fetch(:migration_role) do
+#       within release_path do
+#         with rails_env: fetch(:rails_env) do
+#           execute :rake, "db:seed"
+#         end
+#       end
+#     end
+#   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+#   after 'deploy:migrate', 'deploy:seed'
+# end
 
-end
-
-namespace :deploy do
-  desc 'Runs rake db:seed for SeedMigrations data'
-  task :seed => [:set_rails_env] do
-    on primary fetch(:migration_role) do
-      within release_path do
-        with rails_env: fetch(:rails_env) do
-          execute :rake, "db:seed"
-        end
-      end
-    end
-  end
-
-  after 'deploy:migrate', 'deploy:seed'
-end
