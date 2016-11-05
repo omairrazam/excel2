dc4289940b02fe1811dc50c1e9810e36a2e9b3d7
Omair <omair@gems.techverx.com>
2016-11-05 11:34:38 +0500

    same as last

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 set :keep_releases, 5
 set :rvm_type, :user
 #set :rvm_ruby_version, 'jruby-1.7.19' # Edit this if you are using MRI Ruby
-set :bundle_binstubs, nil
+#set :bundle_binstubs, nil
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
