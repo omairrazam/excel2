b80dd64fc23dd1a6467d710f19b27766eeb29895
Omair <omair@gems.techverx.com>
2016-11-16 21:47:57 +0500

    reverting debuggin

--- app/mailers/sensor_mailer.rb
+++ app/mailers/sensor_mailer.rb
@@ -7,8 +7,8 @@ class SensorMailer < ApplicationMailer
       @date  = Time.zone.now.strftime("%Y/%m/%d")
       @hour  = ((Time.zone.now.strftime("%H")).to_i - 1 ).to_s
 
-      @date = "2016-11-16"
-      @hour = "17"
+      #@date = "2016-11-16"
+      #@hour = "17"
 
       mail(to:@user.email, subject: "#{@user.username.capitalize}'s Machine Updates at #{Time.zone.now} from localmachine", cc: ["omairr.azam@gmail.com","niktrychill@gmail.com"])
     end
