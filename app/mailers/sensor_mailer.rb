308b3685d5512b587b562fc632aa56f18579eecd
Omair <omair@gems.techverx.com>
2016-11-27 21:43:47 +0500

    debugging mailer issue

--- app/mailers/sensor_mailer.rb
+++ app/mailers/sensor_mailer.rb
@@ -4,11 +4,14 @@ class SensorMailer < ApplicationMailer
 
 	def sample_email(user)
       @user  = user
-      @date  = Time.zone.now.strftime("%Y-%m-%d").to_s
-      @hour  = ((Time.zone.now.strftime("%H")).to_i - 1 ).to_s
+      #@date  = Time.zone.now.strftime("%Y-%m-%d").to_s
+      #@hour  = ((Time.zone.now.strftime("%H")).to_i - 1 ).to_s
 
-      @date = "2016-11-27"
-      @hour = "19"
+      @date = 1.hour.ago.strftime("%Y-%m-%d")
+      @hour = 1.hour.ago.strftime("%H")
+      
+      #@date = "2016-11-27"
+      #@hour = "19"
 
       mail(to:@user.email, subject: "#{@user.username.capitalize}'s Machine Updates at #{Time.zone.now}", cc: ["omairr.azam@gmail.com","niktrychill@gmail.com"])
     end
