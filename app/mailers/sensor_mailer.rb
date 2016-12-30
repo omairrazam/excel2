f8f6452dfa375214a5cf052c5557ddf04f8a7210
Omair <omair@gems.techverx.com>
2016-12-31 00:12:05 +0500

    using sedition current occurrence as current time

--- app/mailers/sensor_mailer.rb
+++ app/mailers/sensor_mailer.rb
@@ -2,18 +2,18 @@ class SensorMailer < ApplicationMailer
 	#default to: User.users_with_reports.pluck(:email),
 	default from: "sensordatain@gmail.com"
 
-	def sample_email(user)
+	def sample_email(user,date_time)
       @user  = user
       #@date  = Time.zone.now.strftime("%Y-%m-%d").to_s
       #@hour  = ((Time.zone.now.strftime("%H")).to_i - 1 ).to_s
 
-      @date = 1.hour.ago.strftime("%Y-%m-%d")
-      @hour = 1.hour.ago.strftime("%H")
+      @date = date_time.strftime("%Y-%m-%d")
+      @hour = date_time.strftime("%H")
       
       #@date = "2016-11-27"
       #@hour = "19"
 
-      mail(to:@user.email, subject: "#{@user.username.capitalize}'s Machine Updates at #{Time.zone.now}", cc: ["omairr.azam@gmail.com","niktrychill@gmail.com"])
+      mail(to:@user.email, subject: "#{@user.username.capitalize}'s Machine Updates at #{date_time}", cc: ["omairr.azam@gmail.com"])
     end
 
 end
--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -4,24 +4,27 @@ class PygmentsWorker
 
   recurrence do
     hourly(1)
+    #minutely(2)
     # if you want to specify the exactly minute or minutes
     # `minute_of_hour(30, ...)`
     # hourly(2).minute_of_hour(30)
   end
 
-  def perform
-    u = User.find_by_email("niktrychill@gmail.com")
+  def perform(last_occurrence, current_occurrence)
+    
+    current_date_time = DateTime.strptime(current_occurrence.to_s,'%s')
+    #u = User.find_by_email("niktrychill@gmail.com")
 
-    SensorMailer.sample_email(u).deliver
+    #SensorMailer.sample_email(u).deliver
 
-    # users_with_reports = User.users_with_reports.count
-    # if users_with_reports > 0
-    #   users = User.all
-    #   users.each do |user|
-    #     if user.machines.count > 0
-    #         SensorMailer.sample_email(user).deliver
-    #     end
-    #   end
-    # end
+    users_with_reports = User.users_with_reports.count
+    if users_with_reports > 0
+      users = User.all
+      users.each do |user|
+        if user.machines.count > 0
+            SensorMailer.sample_email(user,current_date_time).deliver
+        end
+      end
+    end
   end
 end
